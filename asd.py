493e075f0db9dd3cebdb6eeb2763bbca979547b1
deyfh <zf@stumail.ysu.edu.cn>
2020-03-25 09:12:07 +0800

    no use

--- asd.py
+++ asd.py
@@ -4,3 +4,6 @@ import tensorflow.keras as keras
 def model():
     txt_layer = keras.layers.Embedding(input_dim=1, output_dim=2, input_length=45,
                                        trainable=False, name='Embedding')
+    a = 2
+    d = 5
+    dd =5
